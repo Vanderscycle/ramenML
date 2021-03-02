95ffc97b89a6f028a4a4925de2215ad1ac056a73
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-03-01 17:19:36 -0800

    init Django, populated the front end slightly, created the pages links. Need to create form for the ml model.

---
+++ src/deployment/pages/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home_view(request,*args, **kwargs):
+    """
+    Home page view (not sure what to display yet)
+    """
+    return render(request,'home.html',{})
+# we want to add a quick story
+def about_view(request,*args, **kwargs):
+    """
+    page view to display the author of the model
+    """
+    context = {
+        'author1':'Henri Vandersleyen',
+        'github1':'https://github.com/Vanderscycle',
+        'author2':'Olivia Kim',
+        'github2':'https://github.com/yjik122'
+    }
+    return render(request,'about.html',context)
+
+def ramenModel_view(request,*args, **kwargs):
+    """
+    We want to set up the possibility to leave a message.
+    We also want them to find a copy of my CV
+    """
+    context = {        
+    }
+    return render(request,'ramenML.html',context)
